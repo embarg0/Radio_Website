71965deb89b3b79f5e507abff31747d3999bce8c
Kat Drobnjakovic <kat.drobnjakovic@gmail.com>
2014-03-29 21:44:10 -0400

    Add constraints

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,3 +1,6 @@
 class Host < ActiveRecord::Base
 	has_and_belongs_to_many :shows
+
+	validates :firstName, :lastName, :stageName, :dateOfBirth, :rating, 
+				:contractStartDate, :salary, :presence => true;
 end
