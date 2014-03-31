8e59e202836566f1aafdf4a10ebfd85ea3504efd
Emilienne <emilienne.pugin@gmail.com>
2014-03-31 03:25:38 -0400

    Query placeholders and query 4m

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,30 @@ class Show < ActiveRecord::Base
 	has_many :time_slots
 	has_and_belongs_to_many :hosts
 	belongs_to :host_shows
+
+	def query4a(userChoice) # Add to the query!
+		query = "SELECT * FROM hosts WHERE(hosts.id IN (1))"
+        results = ActiveRecord::Base.connection.execute(query);
+	end
+
+	def query4b(category, date)# Add to the query!
+		query = "SELECT * FROM hosts WHERE(hosts.id IN (1))"#Select * from tracks where play_sheet_id IN (Select play_sheet_id from play_sheets
+															#where play_sheet_date = '2014-03-04');
+        results = ActiveRecord::Base.connection.execute(query);
+	end
+
+	def query4c(category)
+		query = "SELECT * FROM hosts WHERE(hosts.id IN (1))"
+        results = ActiveRecord::Base.connection.execute(query);
+	end
+
+	def query4d(show)
+		query = "SELECT * FROM hosts WHERE(hosts.id IN (1))"
+        results = ActiveRecord::Base.connection.execute(query);
+	end
+
+	def query4e(category)
+		query = "SELECT * FROM hosts WHERE(hosts.id IN (1))"
+        results = ActiveRecord::Base.connection.execute(query);
+	end
 end
